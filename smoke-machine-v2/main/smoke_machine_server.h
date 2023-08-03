fe2b46bdcf92c8b34eb6127d490b7327338db3e0
NaKyle <nakyle.wright@gmail.com>
2023-08-03 05:04:38 -0500

    Remove obsolete ESP8266 smoke machine controller code.

--- smoke-machine-v2/main/smoke_machine_server.h
+++
@@ -1,6 +0,0 @@
-#ifndef SMOKE_MACHINE_SERVER_H
-#define SMOKE_MACHINE_SERVER_H
-
-void smoke_machine_server_init(void);
-
-#endif // SMOKE_MACHINE_SERVER_H
--- smoke-machine-v2/main/smoke_machine_wifi.h
+++
@@ -1,6 +0,0 @@
-#ifndef SMOKE_MACHINE_WIFI_H
-#define SMOKE_MACHINE_WIFI_H
-
-void smoke_machine_wifi_init(void);
-
-#endif // SMOKE_MACHINE_WIFI_H
