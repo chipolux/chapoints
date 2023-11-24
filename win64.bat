6c53e6fb903de0594dc8cf16075069e43aaeb9d5
NaKyle <nakyle.wright@gmail.com>
2023-11-24 01:10:10 -0600

    Update to latest build script that finds new versions of all tools.

--- win64.bat
+++ win64.bat
@@ -1,35 +1,110 @@
 @echo off
+
+set PROJECT_NAME=chap
+set EXECUTABLE_NAME=%PROJECT_NAME%
+set INSTALLER_NAME=
+set PACKAGE_NAME=
+set ZIPFILE_NAME=
+
+echo Looking up latest Qt version...
+set QT_PREFIX=C:\Qt
+set QT_SUFFIX=msvc2019_64\bin
+for /F "eol=| delims=" %%I in ('dir %QT_PREFIX%\6.* /AD /B /O-N 2^>nul') do (
+    if exist "%QT_PREFIX%\%%I\%QT_SUFFIX%\qmake.exe" (
+        set QT_BIN=%QT_PREFIX%\%%I\%QT_SUFFIX%
+        goto :FOUNDQT
+    )
+)
+goto :NOQTFOUND
+:FOUNDQT
+
+echo Looking up vcvarsall...
+set VC_PREFIX=C:\Program Files (x86)\Microsoft Visual Studio\2019
+set VC_SUFFIX=VC\Auxiliary\Build\vcvarsall.bat
+for /F "eol=| delims=" %%I in ('dir "%VC_PREFIX%\*" /AD /B /O-N 2^>nul') do (
+    if exist "%VC_PREFIX%\%%I\%VC_SUFFIX%" (
+        set VC_VARS="%VC_PREFIX%\%%I\%VC_SUFFIX%"
+        goto :FOUNDVC
+    )
+)
+goto :NOVCFOUND
+:FOUNDVC
+
+if "%INSTALLER_NAME%" == "" goto :FOUNDBC
+echo Looking up Qt Installer Framework...
+set BC_PREFIX=%QT_PREFIX%\Tools\QtInstallerFramework
+set BC_SUFFIX=bin
+for /F "eol=| delims=" %%I in ('dir "%BC_PREFIX%\*" /AD /B /O-N 2^>nul') do (
+    if exist "%BC_PREFIX%\%%I\%BC_SUFFIX%\binarycreator.exe" (
+        set BINARYCREATOR=%BC_PREFIX%\%%I\%BC_SUFFIX%\binarycreator.exe
+        goto :FOUNDBC
+    )
+)
+goto :NOBCFOUND
+:FOUNDBC
+
 set SCRIPT_DIR=%~dp0
-set VC_BIN=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build
-set QT_BIN=C:\Qt\6.5.1\msvc2019_64\bin
 set QT_TOOLCHAIN=%QT_BIN%\..\lib\cmake\Qt6\qt.toolchain.cmake
-set PATH=%QT_BIN%;%PATH%
+set CMAKE_BIN=%QT_PREFIX%\Tools\CMake_64\bin
+set CMAKE=%CMAKE_BIN%\cmake.exe
+set PATH=%QT_BIN%;%CMAKE_BIN%;%PATH%
 
-call "%VC_BIN%\vcvarsall.bat" x64
+echo Setting up MSVC environment...
+call %VC_VARS% x64
 
-:: cleanup build dir and prep for new build
-rmdir build-win64 /s /q  2> nul
+echo Cleaning up...
+rmdir /s /q build-win64 2> nul
 mkdir build-win64
 cd build-win64
 
-:: build application
-cmake -DCMAKE_TOOLCHAIN_FILE="%QT_TOOLCHAIN%" ..\chap
-cmake --build . --config MinSizeRel
+echo Compiling...
+%CMAKE% -DCMAKE_TOOLCHAIN_FILE="%QT_TOOLCHAIN%" ^
+        -DCMAKE_BUILD_TYPE=MinSizeRel ^
+        ..\%PROJECT_NAME%
+%CMAKE% --build . --config MinSizeRel
 
-:: prep for windeployqt
-del /f /q MinSizeRel\*.lib
-del /f /q MinSizeRel\*.exp
-
-:: bring in dependencies with windeployqt and zip it all up
-del /f /q MinSizeRel\*.pdb
-del /f /q MinSizeRel\*.lib
-del /f /q MinSizeRel\*.exp
-del /f /q MinSizeRel\*.manifest
+echo Pulling in dependencies...
+del /f /q MinSizeRel\*.pdb  2> nul
+del /f /q MinSizeRel\*.lib  2> nul
+del /f /q MinSizeRel\*.exp  2> nul
+del /f /q MinSizeRel\*.manifest  2> nul
 %QT_BIN%\windeployqt.exe ^
     --release ^
-    --qmldir ..\chap ^
-    MinSizeRel\chap.exe
-..\tools\7za.exe a -tzip chap.zip .\MinSizeRel\*
+    --qmldir ..\%PROJECT_NAME% ^
+    MinSizeRel\%EXECUTABLE_NAME%.exe
 
 cd /d %SCRIPT_DIR%
+
+if not "%ZIPFILE_NAME%" == "" (
+    .\tools\7za.exe a -tzip %PROJECT_NAME%.zip .\build-win64\MinSizeRel\*
+)
+
+if not "%INSTALLER_NAME%" == "" (
+    echo Cleaning up installer folder...
+    del /f /q installer\%INSTALLER_NAME%  2> nul
+    rmdir /s /q installer\packages\%PACKAGE_NAME%\data  2> nul
+    mkdir installer\packages\%PACKAGE_NAME%\data
+    echo Moving in package data...
+    robocopy build-win64\MinSizeRel installer\packages\%PACKAGE_NAME%\data ^
+             /S /NFL /NDL /NJH /NJS /NP /NS /NC
+    echo Building installer...
+    %BINARYCREATOR% --offline-only ^
+                    -c installer\config\config.xml ^
+                    -p installer\packages ^
+                    installer\%INSTALLER_NAME%
+)
+
 echo Done!
+exit /b
+
+:NOQTFOUND
+echo No valid Qt install found!
+exit /b -1
+
+:NOVCFOUND
+echo No valid Visual C++ install found!
+exit /b -1
+
+:NOBCFOUND
+echo No valid Qt installer framework found!
+exit /b -1
